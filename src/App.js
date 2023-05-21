30f9f8cc6a7b61632e648fa496f74711a5d2c162
Siddharth Shah <siddharthshah1102@gmail.com>
2023-05-21 16:38:58 +0530

    get data from json-server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-export default function App() {
+import { useEffect, useState } from "react";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/categories")
+    .then((response) => response.json())
+    .then((data) => setItems(data));
+  }, []);
+
   return (
-    <h1>Hello World</h1>
+    <div className="App">
+      {
+        items.map((item) => {
+          return (<div key={item.id}>{item.title}</div>);
+        })
+      }
+    </div>
   )
 }
+
+export default App;
